592bd981483f5ef8fb7a037b46a62581ad0efc28
Mauri10G <mauriguevara12@gmail.com>
2026-03-25 23:03:31 -0500

    actuaal

--- Clean Teeth.Domain/Entities/Appointment.cs
+++ Clean Teeth.Domain/Entities/Appointment.cs
@@ -17,17 +17,30 @@ public class Appointment
     public Dentist? Dentist { get; private set; }
     public DentalOffice? DentalOffice { get; private set; }
     public AppointmentType Type { get; private set; } // Tipo de cita
-    public Appointment(Guid patientId, Guid dentistId, Guid dentalOfficeId , TimeInterval timeInterval)
+    public Appointment(Guid patientId, Guid dentistId, Guid dentalOfficeId , TimeInterval timeInterval, AppointmentType type)
     {
         if(timeInterval.Start < DateTime.UtcNow)
         {
             throw new BusinessRuleException($"La fecha de inicio no puede ser anterior a la fecha actual");
+        }
+        
+        // Validacion de los 30 minutos
+        if (type == AppointmentType.Treatment)
+        {
+            // Calculamos la diferencia entre fin e inicio
+            TimeSpan duration = timeInterval.End - timeInterval.Start;
+
+            if (duration.TotalMinutes < 30)
+            {
+                throw new BusinessRuleException("Una cita de tipo Tratamiento debe durar al menos 30 minutos.");
+            }
         }
 
         PatientId = patientId;
         DentistId = dentistId;
         DentalOfficeId = dentalOfficeId;
         TimeInterval = timeInterval;
+        Type = type;
         Status = AppointmentStatus.Scheduled;
         Id = Guid.CreateVersion7();
     }
